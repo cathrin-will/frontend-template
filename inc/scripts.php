64e2e897c8415cc9c7fd1f9b0f6cba4b8a80a893
Ann-Cathrin Will <cathrin.will@gmail.com>
2017-02-02 11:19:04 +0000

    ga warning

--- inc/scripts.php
+++ inc/scripts.php
@@ -1,3 +1,6 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="/dist/js/vendor/jquery.min.js"><\/script>')</script>
 	<script src="/dist/js/main.min.js"></script>
+	<script>
+		console.log("No GA tracking code added");
+	</script>
