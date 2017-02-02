48540392a54b7d11f523667e35ac211dea263d7c
Ann-Cathrin Will <cathrin.will@gmail.com>
2017-02-02 10:25:47 +0000

    jQuery updated

--- inc/scripts.php
+++ inc/scripts.php
@@ -1,3 +1,3 @@
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="/dist/js/vendor/jquery.min.js"><\/script>')</script>
 	<script src="/dist/js/main.min.js"></script>
