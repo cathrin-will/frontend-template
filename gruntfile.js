45979a60016c8670a4c45a0dca153107da306c70
Ann-Cathrin Will <cathrin.will@gmail.com>
2017-06-15 11:28:59 +0100

    now there's a package lock i guess

--- gruntfile.js
+++ gruntfile.js
@@ -36,7 +36,7 @@ module.exports = function(grunt) {
 				processors: [
 					require('pixrem')(), // add fallbacks for rem units
 					require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
-					require('cssnano')({ zindex: false }) // minify the result
+					require('cssnano')({ zindex: false }), // minify the result
 				]
 			},
 			files: {
